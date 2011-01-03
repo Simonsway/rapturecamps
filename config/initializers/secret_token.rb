03140be45e54d6f4cfcb02c4c4aee243e9f787fe
Simonsway <simon.eiler@simonsway.com>
2011-01-03 18:31:32 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rapturecamps::Application.config.secret_token = '0eb0da3152fe19938eed542cc25639ccac87d0e080e28442d2c17ab6b180dbdb96657bf8e9f0763c89a3afaf04ba97c33c0a5a2049262edb694b6fe94c161452'
