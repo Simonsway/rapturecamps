005fb690b384c2b59467cfc3844b45cc7de18ab2
Leon <leon_pacyfier@mail.ru>
2011-02-11 18:46:16 +0200

    fix

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -7,9 +7,6 @@ class Customer < ActiveRecord::Base
   validates :country, :presence => true
   validates :postcode, :presence => true
   validates :tel, :presence => true
-  validates :email, :length => {:minimum => 6, :maximum => 254},
-                    :uniqueness => true,
-                    :format => {:with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i}
   
   def self.for_select
     find(:all).collect{|u| [u.name, u.id]}
